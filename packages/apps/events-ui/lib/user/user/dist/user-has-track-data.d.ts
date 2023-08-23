a637346c59afda100b03ee3468b00ff021cc159e
Cormac Flynn <cormac.flynn@datadoghq.com>
2023-08-23 11:51:11 +0200

    Changes

--- packages/apps/events-ui/lib/user/user/dist/user-has-track-data.d.ts
+++
@@ -1,4 +0,0 @@
-import type { LoggedUser } from '@authentication-lib/user';
-import type { TRACK } from '@events-ui/constants';
-export declare const hasTrackData: (track: TRACK, user: LoggedUser) => boolean;
-export declare const userHasTrackData: (track: TRACK) => boolean;
